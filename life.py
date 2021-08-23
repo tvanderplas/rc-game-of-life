c730e6d05ae58f177bbe521a52ff314dd1b6b711
Space Miner <tchow@tutamail.com>
2021-08-23 12:51:36 -0400

    fix status test (#2)

--- life.py
+++ life.py
@@ -13,17 +13,20 @@ class Life:
     def __init__(self, size=(10, 10)):
         self.board = [[' ' for x in range(size[0])] for y in range(size[1])]
         self.size = size
-        print('\n'.join([str(row) for row in self.board]))
+        
+        
+    def __str__(self):
+        return '\n'.join([str(row) for row in self.board])
 
 
     def neighbors(self, x, y):
         x_range = range(max(x - 1, 0), min(x + 2, self.size[0]))
         y_range = range(max(y - 1, 0), min(y + 2, self.size[1]))
-        return [i for i in product(x_range, y_range) if i != (x, y)]
-
-
-    def status(self, x, y, neighbors):
-        living_neighbors = len([i for i in self.neighbors(x, y) if i == '#'])
+        return [(xx, yy) for (xx, yy) in product(x_range, y_range) if (xx, yy) != (x, y)]
+    
+    
+    def status(self, x, y):
+        living_neighbors = sum((1 for (xx, yy) in self.neighbors(x, y) if self.board[xx][yy] == '#'))
         if living_neighbors == 3:
             return '#'
         else:
